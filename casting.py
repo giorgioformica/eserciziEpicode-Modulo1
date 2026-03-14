5b465a0001ee918ca5996159a3567461a7e83563
giorgioformica <giorgio.formica@gmail.com>
2026-03-14 12:40:38 +0100

    Esercizio Casting

---
+++ casting.py
@@ -0,0 +1,8 @@
+#Esercizio Casting MODULO I
+n = int(input("Inserisci un numero: "))
+print(float(n))
+print(f"Hai inserito il numero {n}")
+print(f"Hai inserito il numero {n:5.2f}")
+print("Hai inserito il numero {numero:.2f}".format(numero=n))
+print("Hai inserito il numero {:.2f}".format(n))
+print("Hai inserito il numero %.2f  " % n)  
