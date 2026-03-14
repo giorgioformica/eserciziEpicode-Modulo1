63a32a1b04ef9b654af0e20f51d16c1733fe6e5b
giorgioformica <giorgio.formica@gmail.com>
2026-03-14 17:44:01 +0100

    II Esecizio While

--- while.py
+++ while.py
@@ -1,4 +1,4 @@
-# Esercizio ciclo WHILE 
+# Esercizio 1 ciclo WHILE 
 x = 0
 while x <= 0:
     x= int(input("Inserisci un numero positivo: "))
@@ -6,3 +6,16 @@ while x <= 0:
         print(f"Hai inserito il numero positivo {x:.2f}")
     else :
         print("Non ha inserito un numero valido")
+
+# Esercizio 1 ciclo WHILE : calcolare la somma delle cifre di un numero
+
+x = 1
+somma = 0
+n = int(input("Inserisci un numero: "))
+
+while x <= n:
+    somma += x
+    x += 1
+    print(somma)
+
+print(f"La somma di tutte le cifre fino a {n} è {somma}")
